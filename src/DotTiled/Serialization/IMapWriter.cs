0c6c320f3562ef13dc6325d59de1542eab001e88
breadbyte <14045257+breadbyte@users.noreply.github.com>
2025-06-03 20:55:06 +0800

    Add stub interfaces for writing maps and tilesets

---
+++ src/DotTiled/Serialization/IMapWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace DotTiled.Serialization;
+
+/// <summary>
+/// Interface for writing a map to a Stream.
+/// </summary>
+public interface IMapWriter : IDisposable
+{
+  /// <summary>
+  /// Writes a map to a <see cref="Stream"/>Stream</see>.
+  /// </summary>
+  /// <returns>The stream containing the serialized tileset.</returns>
+  Stream WriteMap(Map map);
+}
---
+++ src/DotTiled/Serialization/ITilesetWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace DotTiled.Serialization;
+
+/// <summary>
+/// Interface for writing a tileset to a stream.
+/// </summary>
+public interface ITilesetWriter : IDisposable
+{
+  /// <summary>
+  /// Writes a tileset to a <see cref="Stream">Stream</see>.
+  /// </summary>
+  /// <returns>The stream containing the serialized tileset.</returns>
+  Stream WriteTileset(Tileset tileset);
+}
